442db68b361ef068002e26f51d651485eb23da09
David Kwan <dwkwan1989@gmail.com>
2019-10-18 21:16:36 +0000

    Update song template and JS for displaying song details

--- web_app/static/scripts/song_page.js
+++ web_app/static/scripts/song_page.js
@@ -4,12 +4,7 @@ let song_api_url = 'http://0.0.0.0:5001/api/v1/songs/' + id
 fetch(song_api_url)
 .then(response => response.json())
 .then(data => {
-  document.getElementById('song_info').insertAdjacentHTML('beforeend', info_HTML(data));
+  document.getElementById('song_info').innerHTML = `${data.artist}, ${data.title}`;
+  document.getElementById('lyrics').innerHTML = `${data.lyrics}`;
 })
 .catch(error => console.error(error))
-function info_HTML(song) {
-  return(
-    `<li>${song.artist}</li>
-      <li>${song.title}</li>
-      <li>${song.lyrics}</li>`)
-}
