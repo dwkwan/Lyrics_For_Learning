e422dfbf66994c3f584487ae4b2f5d0e9c6164f4
David Kwan <dwkwan1989@gmail.com>
2019-10-20 22:45:08 +0000

    Add event listener to fetch info about word selected

--- web_app/static/scripts/song_page.js
+++ web_app/static/scripts/song_page.js
@@ -15,11 +15,28 @@ let song_word_api_url = 'http://0.0.0.0:5001/api/v1/songs/' + id + '/words'
 fetch(song_word_api_url)
 .then(response => response.json())
 .then(data => {
+  console.log(data)
   for (i = 0; i < data.length; i++) {
     item = document.createElement("LI");
     text = document.createTextNode(data[i].text)
     item.appendChild(text)
     document.getElementById('wordlist').appendChild(item);
-  }
-})
+    setupWordFetch(item)
+   }
+  })
 .catch(error => console.error(error))
+function setupWordFetch(word) {
+  word.addEventListener('click', function() {
+    fetch("https://wordsapiv1.p.rapidapi.com/words/" + word.innerText,
+	  {
+	    headers: {
+	      'x-rapidapi-host': "ENTER API_HOST",
+	      'x-rapidapi-key': "ENTER API_KEY"
+	    }
+	  })
+      .then(response => response.json())
+	.then(data => {
+	  console.log(data)
+	})
+	.catch(error => console.error(error))
+  })}
