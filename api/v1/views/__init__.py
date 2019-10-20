4e27b377b28fd44ea6019ebe722d230edea7e177
David Kwan <dwkwan1989@gmail.com>
2019-10-20 00:46:52 +0000

    Add API song_word views and update init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,3 +4,4 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.songs import *
+from api.v1.views.song_word import *
---
+++ api/v1/views/song_word.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defines view functions to handle requests for songs data"""
+
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+from models.song import Song
+app = Flask(__name__)
+
+
+@app_views.route('/songs/<song_id>/words', methods=['GET'], strict_slashes=False)
+def get_words_for_song(song_id):
+    """Retrieves all Song objects from database and returns a list containing
+    all of them"""
+    song = storage.get("Song", song_id)
+    if song is None:
+        return jsonify({}), 404
+    word_list = []
+    for word in song.words:
+        word_list.append(word.to_dict())
+    return jsonify(word_list), 200
