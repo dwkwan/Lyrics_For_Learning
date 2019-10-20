0203eded7137b9dd1d7ac7817b8d95636d67a4bc
David Kwan <dwkwan1989@gmail.com>
2019-10-20 00:44:13 +0000

    Add Word class and setup many-to-many relationship with Song class

--- models/song.py
+++ models/song.py
@@ -3,11 +3,23 @@
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import *
+
+metadata = Base.metadata
+
+song_word = Table('song_word', metadata,
+            Column('song_id', String(60), ForeignKey("songs.id"),
+            primary_key=True, nullable=False),
+            Column('word_id', String(60), ForeignKey("words.id"), primary_key=True, nullable=False))
+
 
 class Song(BaseModel, Base):
 
-    __tablename__ = 'song'
+    __tablename__ = 'songs'
     artist = Column(String(128), nullable=False)
     title = Column(String(128), nullable=False)
     lyrics = Column(String(5000), nullable=False)
+    genre = Column(String(128), nullable=False)
+    words = relationship("Word", secondary=song_word, viewonly=False)
---
+++ models/word.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module creates the Song class"""
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import *
+
+class Word(BaseModel, Base):
+
+    __tablename__ = 'words'
+    text = Column(String(128), nullable=False)
+    songs = relationship("Song", secondary='song_word', viewonly=False)
