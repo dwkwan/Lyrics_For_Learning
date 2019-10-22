1ba58e1e7eee20b6e9c021d0560857c974ebb5eb
David Kwan <dwkwan1989@gmail.com>
2019-10-22 22:15:07 +0000

    Add GET, PUT, POST endpoints for Interpretation class

---
+++ api/v1/views/interpretations.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""defines view functions to handle requests for interpretations data"""
+
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+from models.song import Song
+from models.word import Word
+from models.interpretation import Interpretation
+app = Flask(__name__)
+
+
+@app_views.route('/interpretations/<word_id>/<song_id>', methods=['GET'], strict_slashes=False)
+def get_interpretations(word_id=None, song_id=None):
+    """Retrieves all Interpretation objects for a word from a song and returns a list containing
+    all of them"""
+    word = storage.get('Word', word_id)
+    if word is None:
+        print("WORD NOT FOUND")
+        abort(404)
+    print("WORD FOUND")
+    song = storage.get('Song', song_id)
+    if song is None:
+        print("SONG NOT FOUND")
+        abort(404)
+    print("SONG FOUND")
+    interpretations_dict = storage.all(Interpretation)
+    interpretations_list = []
+    for interpretation in interpretations_dict.values():
+        if interpretation.word_id == word_id and interpretation.song_id == song_id:
+            interpretations_list.append(interpretation.to_dict())
+    return jsonify(interpretations_list), 200
+
+@app_views.route('/interpretations/<word_id>/<song_id>', methods=['POST'],
+                 strict_slashes=False)
+def post_interpretation(word_id=None, song_id=None):
+    """Creates an interpretation for a word from a song"""
+    print(word_id)
+    print(song_id)
+    word = storage.get('Word', word_id)
+    if word is None:
+        abort(404)
+    song = storage.get('Song', song_id)
+    if song is None:
+        abort(404)
+    result = request.get_json()
+    if result is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'text' not in result:
+        return jsonify({"error": "Missing text"}), 400
+    interpretation_obj = Interpretation(word_id = word_id, song_id=song_id)
+    setattr(interpretation_obj, "text", result["text"])
+    storage.new(interpretation_obj)
+    storage.save()
+    return jsonify(interpretation_obj.to_dict()), 201
+
+@app_views.route('/interpretations/<interpretation_id>', methods=['PUT'], strict_slashes=False)
+def put_interpretation(interpretation_id=None):
+    """Updates an Interpretation object"""
+    interpretation_obj = storage.get('Interpretation', interpretation_id)
+    if interpretation_obj is None:
+        abort(404)
+    result = request.get_json()
+    if result is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'likes' not in result:
+        return jsonify({"error": "Missing likes"}), 400
+    setattr(interpretation_obj, "likes", result["likes"])
+    storage.save()
+    return jsonify(interpretation_obj.to_dict()), 200
