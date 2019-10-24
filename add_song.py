01eb6615d3875382434408395c2294ed1ef4d626
David Kwan <dwkwan1989@gmail.com>
2019-10-24 01:45:40 +0000

    Add test script for adding songs to database

---
+++ add_song.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Test creation of song"""
+import models, requests
+from models.base_model import BaseModel
+from models.song import Song
+from models.word import Word
+from models import storage
+
+input_artist = input('Artist: ')
+input_song = input('Song: ')
+db_songs = {}
+for song in storage.all(Song).values():
+    db_songs[song.title] = song
+if db_songs.get(input_song) is None:
+    input_genre = input('Genre: ')
+    lyrics_dict = requests.get('https://api.lyrics.ovh/v1/{:}/{:}'.format(input_artist, input_song)).json()
+    lyrics = lyrics_dict.get('lyrics')
+    song = Song()
+    song.artist = input_artist
+    song.title = input_song
+    song.lyrics = lyrics
+    song.genre = input_genre
+else:
+    song = db_songs[input_song]
+words = input('words: ')
+word_list = map(str, words.strip('[]').split(','))
+db_words = {}
+for word in storage.all(Word).values():
+    db_words[word.text] = word
+for item in word_list:
+    if db_words.get(item) is None:
+        word = Word()
+        word.text = item
+        word.save()
+        song.words.append(word)
+    else:
+        song.words.append(db_words[item])
+models.storage.new(song)
+song.save()
